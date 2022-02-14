9ef7cc8f32cedc95b47986540511f0179015202d
Chris Bratlien <chris.bratlien@praxent.com>
2022-02-14 12:25:43 -0600

    cb learning SRP

---
+++ ArdalisRating/ConsoleLogger.cs
@@ -0,0 +1,8 @@
+using System;
+namespace ArdalisRating {
+    public class ConsoleLogger {
+        public void Log( string message) {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ ArdalisRating/JsonPolicySerializer.cs
@@ -0,0 +1,10 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace ArdalisRating {
+    public class JsonPolicySerializer {
+        public Policy GetPolicyFromJsonString( string jsonString) {
+            return JsonConvert.DeserializeObject<Policy>(jsonString, new StringEnumConverter());
+        }
+    }
+}
